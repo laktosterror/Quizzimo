84cb8a3d05e9188e083bd10850535dece9d51a3e
laktosterror <55037659+laktosterror@users.noreply.github.com>
2024-10-26 02:27:24 +0200

    new gui  made

--- QuizWPF/ViewModel/MainWindowViewModel.cs
+++ QuizWPF/ViewModel/MainWindowViewModel.cs
@@ -1,17 +1,47 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using QuizWPF.Command;
 using QuizWPF.Model;
+using QuizWPF.View;
 
 namespace QuizWPF.ViewModel;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public ObservableCollection<QuestionPackViewModel> Packs { get; set; }
+    public MainWindowViewModel()
+    {
+        ConfigurationViewModel = new ConfigurationViewModel(this);
+        
+        PlayerViewModel = new PlayerViewModel(this);
+        
+        ActivePack = new QuestionPackViewModel(new QuestionPack("My Question Pack"));
 
+        CurrentView = new HomeView();
+
+        HomeButtonCommand = new DelegateCommand(HomeButton);
+        PlayButtonCommand = new DelegateCommand(PlayButton);
+        ExitButtonCommand = new DelegateCommand(ExitButton);
+
+
+    }
+    public ObservableCollection<QuestionPackViewModel> Packs { get; set; }
     public ConfigurationViewModel ConfigurationViewModel { get; }
     public PlayerViewModel? PlayerViewModel { get; }
 
     private QuestionPackViewModel? _activePack;
-    
+
+    private UserControl _currentView;
+    public UserControl CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            RaisePropertyChanged();
+        }
+    }
+
     public QuestionPackViewModel? ActivePack
     {
         get => _activePack;
@@ -22,12 +52,27 @@ public class MainWindowViewModel : ViewModelBase
         }
     }
 
-    public MainWindowViewModel()
+    public DelegateCommand HomeButtonCommand { get; }
+    public DelegateCommand PlayButtonCommand { get; }
+    public DelegateCommand ExitButtonCommand { get; }
+
+
+    private bool CanUpdateButton()
     {
-        ConfigurationViewModel = new ConfigurationViewModel(this);
-        
-        PlayerViewModel = new PlayerViewModel(this);
-        
-        ActivePack = new QuestionPackViewModel(new QuestionPack("My Question Pack"));
+        return true;
+    }
+
+    private void HomeButton(object obj)
+    {
+        CurrentView = new HomeView();
+    }  
+    private void PlayButton(object obj)
+    {
+        CurrentView = new PlayerView();
+    }
+
+    private void ExitButton(object obj)
+    {
+        Application.Current.Shutdown(); // This will close the application
     }
 }
