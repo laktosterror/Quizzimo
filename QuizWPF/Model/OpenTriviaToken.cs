51f4736106a42129164be22ae3acf2f2ffc62cff
laktosterror <55037659+laktosterror@users.noreply.github.com>
2024-11-13 01:45:59 +0100

    remove unused class

--- QuizWPF/Model/OpenTriviaToken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QuizWPF.Model;
-
-public class OpenTriviaToken
-{
-    public int response_code { get; set; }
-    public string response_message { get; set; }
-    public string token { get; set; }
-}
